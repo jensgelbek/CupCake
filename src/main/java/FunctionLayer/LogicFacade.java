420fe661b97c2c6d235bb9cd14c1172155135b25
jensgelbek <jensgelbek@gmail.com>
2020-10-21 11:19:28 +0200

    fewr

--- src/main/java/FunctionLayer/LogicFacade.java
+++ src/main/java/FunctionLayer/LogicFacade.java
@@ -2,6 +2,9 @@ package FunctionLayer;
 
 import DBAccess.UserMapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The purpose of LogicFacade is to...
  * @author kasper
@@ -16,6 +19,21 @@ public class LogicFacade {
         User user = new User(email, password, "customer");
         UserMapper.createUser( user );
         return user;
+    }
+    public static List<Bottom> getAllBottoms (){
+        List<Bottom> retval=new ArrayList<Bottom>();
+        retval.add(new Bottom(1,"bottom1",5));
+        retval.add(new Bottom(2,"bottom2",5));
+        return retval;
+
+    }
+
+    public static List<Top> getAllToppings (){
+        List<Top> retval=new ArrayList<Top>();
+        retval.add(new Top(1,"top1",5));
+        retval.add(new Top(2,"top2",5));
+        return retval;
+
     }
 
 }
