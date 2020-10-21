8678d54a41bbce8d0997cc578ef278844cae22c8
jensgelbek <jensgelbek@gmail.com>
2020-10-21 09:34:06 +0200

    nu det hele

--- src/test/OneTimeSetup.sql
+++ src/test/OneTimeSetup.sql
@@ -1,9 +1,11 @@
 -- There is a schema named "useradmin" with one table: "Users"
 -- The test schema is named "useradminTest"
 -- make a copy of the table
-CREATE TABLE useradminTest.Users LIKE useradmin.Users;
+CREATE DATABASE  IF NOT EXISTS cupcakeTest;
+use cupcakeTest;
+CREATE TABLE cupcakeTest.Users LIKE cupcake.Users;
 -- make an other copy, this time naming it UsersTest
-CREATE TABLE UsersTest LIKE useradminTest.Users;
+CREATE TABLE UsersTest LIKE cupcakeTest.Users;
 INSERT INTO `UsersTest` VALUES 
     (1,'jens@somewhere.com','jensen','customer'),
     (2,'ken@somewhere.com','kensen','customer'),
@@ -12,4 +14,4 @@ INSERT INTO `UsersTest` VALUES
 
 -- make a new user for the test database
 CREATE USER 'testinguser' IDENTIFIED BY 'try1try2tryAgain';
-GRANT ALL PRIVILEGES ON useradminTest.* TO 'testinguser';
+GRANT ALL PRIVILEGES ON cupcakeTest.* TO 'testinguser';
